46d7cdb5d0776a6bcb903c8f6ddb4b3448e0b7dd
Eyad Wahdan <Eyadwahdan02@gmail.com>
2021-10-16 13:27:51 +0200

    Initial commit

---
+++ src/test/java/htw/berlin/webtech/PlanIt/PlanItApplicationTests.java
@@ -0,0 +1,13 @@
+package htw.berlin.webtech.PlanIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
