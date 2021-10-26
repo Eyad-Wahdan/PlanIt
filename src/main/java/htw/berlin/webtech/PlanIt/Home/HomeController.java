02fb2fa9726a115cde721bff9ac64511c64884b5
Eyad Wahdan <Eyadwahdan02@gmail.com>
2021-10-26 17:27:35 +0200

    Parameter added  - query string

--- src/main/java/htw/berlin/webtech/PlanIt/Home/HomeController.java
+++ src/main/java/htw/berlin/webtech/PlanIt/Home/HomeController.java
@@ -1,7 +1,9 @@
 package htw.berlin.webtech.PlanIt.Home;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
@@ -9,6 +11,12 @@ public class HomeController {
     @GetMapping("/")
     public String handleBase() {
         return "Home";
+    }
+
+    @GetMapping("/{name}")
+    public String greeting(@RequestParam(name="name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "Greeting";
     }
 
 }
